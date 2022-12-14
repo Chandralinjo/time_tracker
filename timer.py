f91500fae7f2eaf0c4289fa7ed6aef09ec0f8d50
Chandralinjo <boris.majic@gmail.com>
2022-12-15 00:36:03 +0100

    Set up for advanced features

--- timer.py
+++ timer.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from tkinter import messagebox
 from datetime import datetime
 
 # Define global variables
@@ -40,6 +41,18 @@ def stop_timer():
     with open("time_tracking.txt", "a") as file:
         file.write(task_name.get() + ": " + represent_time(elapsed) + "\n")
 
+def reset_file():
+    # Show the pop-up window with the Yes/No buttons
+    result = tk.messagebox.askyesno("Confirm", "Are you sure you want to clear history?")
+
+    # If the user clicks Yes, show a message
+    if result:
+        tk.messagebox.showinfo("Success", "The action was completed successfully.")
+
+def tidy_up():
+    ## TODO write this function
+    return
+
 def update_timer():
     # Update label periodically
     if update_label:
@@ -49,23 +62,35 @@ def update_timer():
 
 # Create the main window
 window = tk.Tk()
-window.geometry("400x200")
+window.geometry("300x200")
+grid = tk.Frame(window)
 
 # Create a text box to enter the name of a task
 task_name = tk.Entry(window)
-task_name.pack()
+task_name.grid(row=0, columnspan=2)
 
 # Create a button to start the timer
 start_button = tk.Button(window, text="Start", command=start_timer)
-start_button.pack()
+start_button.grid(row=1, column=0)
 
 # Create a button to stop the timer
 stop_button = tk.Button(window, text="Stop", command=stop_timer)
-stop_button.pack()
+stop_button.grid(row=1, column=1)
 
 # Create a label to display the elapsed time
 elapsed_time = tk.Label(window, text="Elapsed time:")
-elapsed_time.pack()
+elapsed_time.grid(row=2, columnspan=2)
+
+# Add two more buttons. One button should perform rounding and scaling for the current week.
+# The second button should format the time_tracking file.
+tidy_button = tk.Button(window, text="Tidy", command=tidy_up)
+tidy_button.grid(row=3, column=0)
+
+clear_button = tk.Button(window, text="Clear history", command=reset_file)
+clear_button.grid(row=3, column=1)
+
+grid.columnconfigure(0, minsize=150)
+grid.columnconfigure(1, minsize=150)
 
 
 # Run the main event loop
